ac764fcfaba2dd63ffcb37f4bef66943585e960b
Santiago Martinez <santiago.mtz.0054@gmail.com>
2025-11-29 20:20:03 -0600

    Limpieza: Eliminando archivos duplicados de la raiz

--- src/routes/productos.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProductos, createProducto } from '../controllers/productos.controller.js';
-
-const router = express.Router();
-
-router.get('/', getProductos);
-router.post('/', createProducto);
-
-export default router;
